a29dc5b07eacb566e771d5d0f87ba8023419407f
totex <tonyfilpo@azet.sk>
2019-12-30 18:00:46 +0100

    camera mouse look

--- TextureLoader.py
+++ TextureLoader.py
@@ -5,6 +5,7 @@ from OpenGL.GL import glBindTexture, glTexParameteri, GL_TEXTURE_2D, GL_TEXTURE_
 from PIL import Image
 
 
+
 def load_texture(path, texture):
     glBindTexture(GL_TEXTURE_2D, texture)
     # Set the texture wrapping parameters
@@ -18,4 +19,22 @@ def load_texture(path, texture):
     image = image.transpose(Image.FLIP_TOP_BOTTOM)
     img_data = image.convert("RGBA").tobytes()
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+    return texture
+
+
+def load_texture_pygame(path, texture):
+    import pygame
+    glBindTexture(GL_TEXTURE_2D, texture)
+    # Set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    # Set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    # load image
+    image = pygame.image.load(path)
+    image = pygame.transform.flip(image, False, True)
+    image_width, image_height = image.get_rect().size
+    img_data = pygame.image.tostring(image, "RGBA")
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
     return texture
