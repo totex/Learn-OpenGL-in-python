91dc0cc72e66534656ec34cddd4ef8b2e9bd57c3
totex <tonyfilpo@azet.sk>
2020-01-10 22:45:40 +0100

    camera WASD movement

--- ep18_camera_WASD.py
+++ ep18_camera_WASD.py
@@ -22,14 +22,22 @@ def key_input_clb(window, key, scancode, action, mode):
 
     if key == glfw.KEY_W and action == glfw.PRESS:
         forward = True
+    elif key == glfw.KEY_W and action == glfw.RELEASE:
+        forward = False
     if key == glfw.KEY_S and action == glfw.PRESS:
         backward = True
+    elif key == glfw.KEY_S and action == glfw.RELEASE:
+        backward = False
     if key == glfw.KEY_A and action == glfw.PRESS:
         left = True
+    elif key == glfw.KEY_A and action == glfw.RELEASE:
+        left = False
     if key == glfw.KEY_D and action == glfw.PRESS:
         right = True
-    if key in [glfw.KEY_W, glfw.KEY_S, glfw.KEY_D, glfw.KEY_A] and action == glfw.RELEASE:
-        left, right, forward, backward = False, False, False, False
+    elif key == glfw.KEY_D and action == glfw.RELEASE:
+        right = False
+    # if key in [glfw.KEY_W, glfw.KEY_S, glfw.KEY_D, glfw.KEY_A] and action == glfw.RELEASE:
+    #     left, right, forward, backward = False, False, False, False
 
 
 # do the movement, call this function in the main loop
